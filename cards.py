63973b634dd0f0ee66b6f6b8f874d5f1a44f82b3
vagrant <vagrant@vagrant.vm>
2017-07-08 16:28:34 +0000

    Finished cards.py

---
+++ cards.py
@@ -0,0 +1,68 @@
+""" The cards for the game. """
+
+
+points = {'Asso': 11, 'Tre': 10, 'Re': 4, 'Cavallo': 3, 'Fante': 2}
+
+
+class Card(object):
+    """ A card for the game. """
+
+    def __init__(self, suit, card):
+        self.suit = suit
+        self.card = card
+        self.points = points.get(card, 0)
+
+    def __repr__(self):
+        return "<%s di %s: %s pts>" % (self.card, self.suit, self.points)
+
+
+class Deck(object):
+    """ A deck of complete cards. """
+
+    def __init__(self):
+        self.cards = [Card('Dinari', 'Asso'),
+                      Card('Dinari', 'Tre'),
+                      Card('Dinari', 'Re'),
+                      Card('Dinari', 'Cavallo'),
+                      Card('Dinari', 'Fante'),
+                      Card('Dinari', '7'),
+                      Card('Dinari', '6'),
+                      Card('Dinari', '5'),
+                      Card('Dinari', '4'),
+                      Card('Dinari', '2'),
+                      # Coppe
+                      Card('Coppe', 'Asso'),
+                      Card('Coppe', 'Tre'),
+                      Card('Coppe', 'Re'),
+                      Card('Coppe', 'Cavallo'),
+                      Card('Coppe', 'Fante'),
+                      Card('Coppe', '7'),
+                      Card('Coppe', '6'),
+                      Card('Coppe', '5'),
+                      Card('Coppe', '4'),
+                      Card('Coppe', '2'),
+                      # Spade
+                      Card('Spade', 'Asso'),
+                      Card('Spade', 'Tre'),
+                      Card('Spade', 'Re'),
+                      Card('Spade', 'Cavallo'),
+                      Card('Spade', 'Fante'),
+                      Card('Spade', '7'),
+                      Card('Spade', '6'),
+                      Card('Spade', '5'),
+                      Card('Spade', '4'),
+                      Card('Spade', '2'),
+                      # Bastoni
+                      Card('Bastoni', 'Asso'),
+                      Card('Bastoni', 'Tre'),
+                      Card('Bastoni', 'Re'),
+                      Card('Bastoni', 'Cavallo'),
+                      Card('Bastoni', 'Fante'),
+                      Card('Bastoni', '7'),
+                      Card('Bastoni', '6'),
+                      Card('Bastoni', '5'),
+                      Card('Bastoni', '4'),
+                      Card('Bastoni', '2')]
+
+    def __repr__(self):
+        return "<Deck>"
